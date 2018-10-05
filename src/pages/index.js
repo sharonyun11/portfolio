c83f0f66f7084d47fd5650079fd7d742fba80e02
Sharon Yun <sharonyun11@gmail.com>
2018-10-04 22:20:32 -0700

    first push

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Sharon Yun</h1>
+    <p>Welcome to my portfolio.</p>
+  </Layout>
+)
+
+export default IndexPage
